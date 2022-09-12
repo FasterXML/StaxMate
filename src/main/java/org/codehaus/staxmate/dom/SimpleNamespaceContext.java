c427d5c7a9ee0df8793c16aad50a52668e261a97
Jürgen Key <jkey@arcor.de>
2022-09-12 16:17:50 +0200

    re #13, re #14

---
+++ src/main/java/org/codehaus/staxmate/dom/SimpleNamespaceContext.java
@@ -0,0 +1,28 @@
+//https://stackoverflow.com/questions/6390339/how-to-query-xml-using-namespaces-in-java-with-xpath
+package org.codehaus.staxmate.dom;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import javax.xml.namespace.NamespaceContext;
+
+public class SimpleNamespaceContext implements NamespaceContext {
+
+	private final Map<String, String> PREF_MAP = new HashMap<String, String>();
+
+	public SimpleNamespaceContext(final Map<String, String> prefMap) {
+		PREF_MAP.putAll(prefMap);
+	}
+
+	public String getNamespaceURI(String prefix) {
+		return PREF_MAP.get(prefix);
+	}
+
+	public String getPrefix(String uri) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Iterator getPrefixes(String uri) {
+		throw new UnsupportedOperationException();
+	}
+}
