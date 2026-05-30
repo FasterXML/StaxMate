8c9a46fdafe7e72f1f069cbec50082edcc9ee1d1
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-05-29 19:06:08 -0700

    Minor fixes

--- src/main/java/org/codehaus/staxmate/util/DataUtil.java
+++ src/main/java/org/codehaus/staxmate/util/DataUtil.java
@@ -1,24 +1,10 @@
 package org.codehaus.staxmate.util;
 
-import java.util.*;
-
 /**
  * Utility class that contains methods for simple data conversions.
  */
 public final class DataUtil
 {
-    final static HashMap<String,Boolean> sBoolValues;
-    static {
-        sBoolValues = new HashMap<String,Boolean>();
-        sBoolValues.put("true", Boolean.TRUE);
-        sBoolValues.put("false", Boolean.FALSE);
-        /* Note: as per XML Schema, "0" (false) and "1" (true) are
-         * also valid boolean values.
-         */
-        sBoolValues.put("0", Boolean.TRUE);
-        sBoolValues.put("1", Boolean.FALSE);
-    }
-
     private DataUtil() { }
 
     /*
